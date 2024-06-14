1a1f9c72a2139a4107a1f09bb059fdce649df5bb
Misael <126593845+Mstriato@users.noreply.github.com>
2024-06-14 13:16:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,55 @@
-// creat web server
-// 1. Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-// 2. Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  var path = url.parse(request.url).pathname;
-  switch (path) {
-    case '/':
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write('<h1>Hello!</h1>');
-      response.end();
-      break;
-    case '/comments':
-      fs.readFile(__dirname + '/comments.json', function (error, data) {
-        if (error) {
-          response.writeHead(404);
-          response.write("opps this doesn't exist - 404");
-          response.end();
-        } else {
-          response.writeHead(200, { 'Content-Type': 'text/json' });
-          response.write(data);
-          response.end();
-        }
-      });
-      break;
-    default:
-      response.writeHead(404);
-      response.write("opps this doesn't exist - 404");
-      response.end();
-      break;
-  }
+// Create web server
+// Create web server
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+
+// Get all comments
+router.get('/comments', function(request, response) {
+    db.query('SELECT * FROM comments', function(error, results, fields) {
+        if (error) throw error;
+        response.send(results);
+    });
 });
-// 3. Listen on port 8000, IP defaults to  
+
+// Get comment by ID
+router.get('/comments/:id', function(request, response) {
+    db.query('SELECT * FROM comments WHERE id=?', [request.params.id], function(error, results, fields) {
+        if (error) throw error;
+        response.send(results);
+    });
+});
+
+// Add a new comment
+router.post('/comments', function(request, response) {
+    let comment = request.body;
+    if (!comment) {
+        response.status(400).send('Request body is missing');
+    }
+    db.query('INSERT INTO comments SET ?', comment, function(error, results, fields) {
+        if (error) throw error;
+        response.send(results);
+    });
+});
+
+// Update comment
+router.put('/comments', function(request, response) {
+    let comment = request.body;
+    if (!comment) {
+        response.status(400).send('Request body is missing');
+    }
+    db.query('UPDATE comments SET body=? WHERE id=?', [comment.body, comment.id], function(error, results, fields) {
+        if (error) throw error;
+        response.send(results);
+    });
+});
+
+// Delete comment
+router.delete('/comments/:id', function(request, response) {
+    db.query('DELETE FROM comments WHERE id=?', [request.params.id], function(error, results, fields) {
+        if (error) throw error;
+        response.send('Comment has been deleted');
+    });
+});
+
+module.exports = router;
